e82a552bfc3969548f92c2c4e10870b03a6464be
pravatmaastrix <pravat.maastrix@gmail.com>
2025-09-08 15:53:58 +0530

    dbconnection conflict issue

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -1,18 +1,18 @@
 var mysql = require("mysql2");
 
-// var connection = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "node_test",
-// });
-
 var connection = mysql.createPool({
   host: "localhost",
-  port: 3306,
-  user: "admin_popnew",
-  password: "S176cje4$",
-  database: "admin_popnew",
+  user: "root",
+  password: "",
+  database: "node_test",
 });
+
+// var connection = mysql.createPool({
+//   host: "localhost",
+//   port: 3306,
+//   user: "admin_popnew",
+//   password: "S176cje4$",
+//   database: "admin_popnew",
+// });
 
 module.exports = connection;
